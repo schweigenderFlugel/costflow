1afa1a7ce63ec50c8012201927b6221207e378f9
CelliUriel <urielcelli@gmail.com>
2025-08-09 23:14:35 -0300

    feat:CRUD for product

---
+++ server/api/routes/product_route.py
@@ -0,0 +1,115 @@
+from fastapi import APIRouter
+from schemas.http_response import Response
+
+router = APIRouter(
+  tags=['Product'],
+  prefix='/product'
+)
+
+@router.get("", summary="Get Products", responses={
+    200: Response(
+        description="Succesfully Response",
+        content_type="application/json",
+        message="Succesfully Response"
+    ).custom_response(),
+    404: Response(
+        description="Product Not Found",
+        content_type="application/json",
+        message="Product Not Found"
+    ).custom_response(),
+    500: Response(
+        description="Internal Server Error", 
+        content_type="application/json",
+        message="Internal Server Error"
+    ).custom_response(), 
+})
+def get_products():
+    return {"Message":"Products Number 1"}
+
+
+@router.post("", summary="Create Products", responses={
+    201: Response (
+        description="Product Created.",
+        content_type="application/json",
+        message="Product Created."
+    ).custom_response(), 
+    400:
+    Response (
+        description="Bad Request",
+        content_type="application/json",
+        message="Bad Request"
+    ).custom_response(),
+    409: Response(
+        description="Conflict",
+        content_type="application/json",
+        message="Conflict"
+    ).custom_response(),
+    500: Response(
+        description="Internal Server Error", 
+        content_type="application/json",
+        message="Internal Server Error"
+    ).custom_response(),
+
+
+}) 
+
+def post_products():
+    return {"Message":"The Products has been created."}
+
+@router.put("", summary="Update Products", responses={
+    200: Response(
+        description="Product Updated.",
+        content_type="application/json",
+        message="Product Updated"
+    ).custom_response(),
+     400:
+    Response (
+        description="Bad Request",
+        content_type="application/json",
+        message="Bad Request"
+    ).custom_response(),
+     404: Response(
+        description="Product Not Found",
+        content_type="application/json",
+        message="Product Not Found"
+    ).custom_response(),
+    403:Response(
+        description="Forbidden",
+        content_type="application/json",
+        message="Forbidden"
+    ).custom_response(),
+       500: Response(
+        description="Internal Server Error", 
+        content_type="application/json",
+        message="Internal Server Error"
+    ).custom_response(), 
+}) 
+
+def put_products():
+     return {"Message":"The Product has updated."}
+
+@router.delete("", summary="Delete Products", responses={
+     200: Response(
+        description="Product Deleted",
+        content_type="application/json",
+        message="Product Deleted"
+    ).custom_response(),
+    404: Response(
+        description="Product Not Found",
+        content_type="application/json",
+        message="Product Not Found"
+    ).custom_response(),
+    403:Response(
+        description="Forbidden",
+        content_type="application/json",
+        message="Forbidden"
+    ).custom_response(),
+     500: Response(
+        description="Internal Server Error", 
+        content_type="application/json",
+        message="Internal Server Error"
+    ).custom_response(), 
+}) 
+
+def delete_products():
+    return {"Message":"The Products has deleted."}
