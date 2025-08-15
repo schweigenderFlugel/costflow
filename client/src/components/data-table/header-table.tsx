fa7084ece92fc098e4070fa941793ab9f30792f4
Maidana <franco_maidana07@hotmail.com>
2025-08-14 21:32:05 -0300

    client: feat: components to data-table

--- client/src/components/data-table/header-table.tsx
+++ client/src/components/data-table/header-table.tsx
@@ -1,34 +1,44 @@
 import ColumnsDropdown from "@/components/data-table/columns-dropdown";
+import CreateFeedstockTrigger from "@/components/feedstock/modal/trigger/create-feedstock-trigger";
+import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { ObjFeedstock } from "@/types/items/feedstock";
 import { Table } from "@tanstack/react-table";
+import { Download, ListFilter } from "lucide-react";
 
 
 const HeaderTable = ({ table }: { table: Table<ObjFeedstock> }) => (
-  <header className="flex items-center justify-between py-4 gap-4 sm:flex-nowrap flex-wrap">
+  <header className="flex items-center justify-between pt-4 pb-6 gap-4 sm:flex-nowrap flex-wrap">
 
-    <div className="flex gap-x-5 gap-y-2 items-center sm:w-auto w-full">
-      <Input
+    <div className="flex gap-2 justify-between items-center sm:w-auto w-full">
+      {/* <Input
         placeholder="Filter sku..."
         value={(table.getColumn("sku")?.getFilterValue() as string) ?? ""}
         onChange={(event) =>
           table.getColumn("sku")?.setFilterValue(event.target.value)
         }
         className="max-w-sm"
-      />
+      /> */}
+      <ColumnsDropdown table={table} />
       <Input
-        placeholder="Filter names..."
+        placeholder="Buscar por nombre..."
         value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
         onChange={(event) =>
           table.getColumn("name")?.setFilterValue(event.target.value)
         }
-        className="max-w-sm"
+        className="w-xs max-w-[calc(100%-10rem)]"
       />
+      <Button type="button" variant={"ghost"} disabled>
+        <ListFilter className="size-full" />
+      </Button>
     </div>
 
-    <div className="flex items-center ml-auto space-x-2">
-      <ColumnsDropdown table={table} />
-      {/* <CreateFeedstockForm /> */}
+    <div className="flex items-center justify-end gap-4 w-full">
+      <Button variant={"outline"} disabled>
+        <Download />
+        Importar
+      </Button>
+      <CreateFeedstockTrigger />
     </div>
   </header >
 )
