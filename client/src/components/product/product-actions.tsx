5377e1c856eed38f3cd78ec3fe13b7ff96710ab8
Maidana <franco_maidana07@hotmail.com>
2025-08-22 17:58:35 -0300

    client: refactor: actions and already components

--- client/src/components/product/product-actions.tsx
+++ client/src/components/product/product-actions.tsx
@@ -2,31 +2,44 @@
 import { Button } from "@/components/ui/button"
 import { useDeleteProductDialog, useUpdateProductDialog } from "@/hooks/use-product-dialog"
 import { ObjProduct } from "@/types/items/product"
+import { fetcher } from "@/utils/fetcher"
 import { Trash2, Pencil } from "lucide-react"
 
 const ProductActions = ({ product }: { product: ObjProduct }) => {
-  const { setIsOpen: setUpdateState, setProduct: setUpdateFeedstock } = useUpdateProductDialog()
+  const { setIsOpen: setUpdateState, setProduct: setUpdateFeedstock, setIsLoadingProduct } = useUpdateProductDialog()
   const { setIsOpen: setDeleteState, setProduct: setDeleteFeedstock } = useDeleteProductDialog()
 
   const handleDelete = () => {
     setDeleteState(true)
     setDeleteFeedstock(product)
-    console.log("Delete product", product.id)
   }
-  const handleUpdate = () => {
+
+  const handleUpdate = async () => {
+    setIsLoadingProduct(true)
     setUpdateState(true)
-    setUpdateFeedstock(product)
-    console.log("Update product", product.id)
+
+    try {
+      const allInfoToProduct = await fetcher({ input: `/api/product/${product.id}` })
+
+      if (!('error' in allInfoToProduct) && !(allInfoToProduct.detail)) {
+        setUpdateFeedstock(allInfoToProduct)
+      }
+    } catch (error) {
+      console.error('Error fetching product details:', error)
+    } finally {
+      setIsLoadingProduct(false)
+    }
   }
 
 
-  return (<div className="flex gap-1 px-1">
+  return (<div className="flex gap-1">
     <Button
       data-slot="product-update-dialog-trigger"
       onClick={handleUpdate}
       type="button"
       className="text-primary hover:text-primary cursor-pointer"
       variant={"ghost"}
+      size={"sm"}
     >
       <Pencil className="size-4" />
     </Button>
@@ -37,6 +50,7 @@ const ProductActions = ({ product }: { product: ObjProduct }) => {
       type="button"
       className="text-red-800 hover:text-red-800 cursor-pointer"
       variant={"ghost"}
+      size={"sm"}
     >
       <Trash2 className="size-4" />
     </Button>
