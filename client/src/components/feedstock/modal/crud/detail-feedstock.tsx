8500002a1486a59efe3186626dbeb2509a5f47ad
Maidana <franco_maidana07@hotmail.com>
2025-08-22 18:00:11 -0300

    client: feat: detail sub component

--- client/src/components/feedstock/modal/crud/detail-feedstock.tsx
+++ client/src/components/feedstock/modal/crud/detail-feedstock.tsx
@@ -7,24 +7,29 @@ const DetailFeedstock = ({ feedstock }: { feedstock: ObjFeedstock | Feedstock | 
   if (feedstock == null) return null;
 
   return (
-    <div className="p-4 w-full mx-auto bg-muted/60 rounded-xs text-muted-foreground">
+    <div className="p-4 mb-12 bg-muted/60 rounded-xs text-muted-foreground">
       <h4 className="text-base font-medium">Detalles del insumo:</h4>
 
       <div className="mt-3 space-y-2">
         <div className="flex justify-between items-center">
-          <span className="text-sm">Nombre:</span>
-          <span className="text-sm font-medium text-foreground">{feedstock.name}</span>
+          <h3 className="text-sm">SKU:</h3>
+          <span className="text-sm font-medium text-foreground text-truncate pl-2" title={feedstock.sku}>{feedstock.sku}</span>
         </div>
 
         <div className="flex justify-between items-center">
-          <span className="text-sm">Proveedor:</span>
-          <span className="text-sm font-medium text-foreground">
+          <h3 className="text-sm">Nombre:</h3>
+          <span className="text-sm font-medium text-foreground text-truncate pl-2" title={feedstock.name}>{feedstock.name}</span>
+        </div>
+
+        <div className="flex justify-between items-center">
+          <h3 className="text-sm">Proveedor:</h3>
+          <span className="text-sm font-medium text-foreground text-truncate pl-2" title={feedstock.provider} >
             {feedstock.provider || "No especificado"}
           </span>
         </div>
 
         <div className="flex justify-between items-center">
-          <span className="text-sm">Costo unitario:</span>
+          <h3 className="text-sm">Costo unitario:</h3>
           <span className="text-sm font-medium text-foreground">
             {
               (new Intl.NumberFormat("es-AR", {
@@ -36,14 +41,14 @@ const DetailFeedstock = ({ feedstock }: { feedstock: ObjFeedstock | Feedstock | 
         </div>
 
         <div className="flex justify-between items-center">
-          <span className="text-sm">Unidad de medida:</span>
+          <h3 className="text-sm">Unidad de medida:</h3>
           <span className="text-sm font-medium text-foreground">
             {translateMeasureUnit(feedstock.measure_unit)}
           </span>
         </div>
 
         <div className="flex justify-between items-center">
-          <span className="text-sm">Agregado en la fecha:</span>
+          <h3 className="text-sm">Fecha de creación:</h3>
           <span className="text-sm font-medium text-foreground">
             {
               'created_at' in feedstock ?
@@ -53,9 +58,22 @@ const DetailFeedstock = ({ feedstock }: { feedstock: ObjFeedstock | Feedstock | 
           </span>
         </div>
 
+        {
+          'created_at' in feedstock &&
+            (feedstock.created_at === feedstock.updated_at) ?
+            <div className="flex justify-between items-center">
+              <h3 className="text-sm">Fecha de modificación:</h3>
+              <span className="text-sm font-medium text-foreground">
+                {
+                  new Date(feedstock.updated_at).toLocaleDateString()
+                }
+              </span>
+            </div>
+            : ""
+        }
 
       </div>
-    </div>
+    </div >
   )
 }
 
