1e74e8dbe3a14e93fa7d4ac22fbd82832cef127f
Maidana <franco_maidana07@hotmail.com>
2025-09-13 16:20:55 -0300

    client: feat: maintenance and 404 pages

--- client/src/app/not-found.tsx
+++ client/src/app/not-found.tsx
@@ -1,23 +1,29 @@
 import Link from "next/link";
+import Image from "next/image";
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
-    return (
-        <div className="min-h-[calc(100svh-8rem)] w-full flex items-center justify-center bg-gray-50">
-            <div className="max-w-md w-full px-6 py-8 text-center">
-                <div className="relative w-fit mx-auto mb-8">
-                    <div className="w-64 h-64 rounded-full bg-black text-white content-center">
-                        <h2 className="font-extrabold text-5xl">404</h2>
-                    </div>
-                </div>
-                <h1 className="text-4xl font-bold text-gray-900 mb-2">Página no encontrada</h1>
-                <p className="text-gray-600 mb-8">
-                    Es posible que la página que buscas haya sido eliminada o no esté disponible temporalmente.
-                </p>
-                <Button asChild>
-                    <Link href="/">Volver al inicio</Link>
-                </Button>
-            </div>
+  return (
+    <div className="pb-10 w-full flex items-center justify-center">
+      <div className="max-w-lg w-full px-2 sm:px-6 py-8 text-center">
+        <div className="relative w-fit mx-auto mb-8">
+          <Image
+            src="/assets/maintenance/not-found.png"
+            alt="404 - Página no encontrada"
+            width={380}
+            height={292}
+            className="w-full h-auto max-w-sm mx-auto"
+            priority
+          />
         </div>
-    );
+        <h1 className="text-2xl font-bold text-[#3B3B3B] mb-2">¡Ups! La página que buscabas no existe.</h1>
+        <p className="text-gray-600 mb-8">
+          Es posible que la página que buscas haya sido eliminada o no esté disponible temporalmente.
+        </p>
+        <Button asChild variant={"link"} className="font-semibold text-lg">
+          <Link href="/">Volver al inicio</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
--- client/src/components/configuration/content/notifications-content.tsx
+++ client/src/components/configuration/content/notifications-content.tsx
@@ -1,20 +1,9 @@
-import Image from "next/image"
+import MaintenancePage from "@/components/maintenance-page"
 
-const HelpContent = () => {
+const NotificationsContent = () => {
   return (
-    <section className="max-w-[calc(100svw-2rem)] w-6xl mx-auto my-8 px-1 sm:px-5 text-center space-y-6">
-      <h3 className="text-2xl font-bold">Notificaciones en desarrollo</h3>
-      <Image
-        src={"https://media.tenor.com/y-XNYFHZLgQAAAAM/homer-simpson.gif"}
-        alt="homer-simpson"
-        width={220}
-        height={165}
-        className="mx-auto"
-        unoptimized
-      />
-      <p className="text-muted-foreground">Esta sección estará disponible próximamente</p>
-    </section>
+    <MaintenancePage />
   )
 }
 
-export default HelpContent
+export default NotificationsContent
